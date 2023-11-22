679d18a0bc663a6bd3a52bfe49f585bd73ad25d3
alihaidar2950 <alihaidar2950@gmail.com>
2023-11-21 20:46:42 -0500

    Update DateCounter.js

--- src/DateCounter.js
+++ src/DateCounter.js
@@ -2,6 +2,9 @@ import { useState, useReducer } from 'react';
 
 function reducer(state, action) {
   console.log(state, action);
+  if (action.type === 'dec') return state - 1;
+  if (action.type === 'inc') return state + 1;
+  if (action.type === 'setCount') return action.payload;
   return state + action;
 }
 
@@ -17,19 +20,19 @@ function DateCounter() {
   date.setDate(date.getDate() + count);
 
   const dec = function () {
-    dispatch({ type: 'dec', payload: -1 });
+    dispatch({ type: 'dec' });
     // setCount((count) => count - 1);
     // setCount((count) => count - step);
   };
 
   const inc = function () {
-    dispatch(1);
+    dispatch({ type: 'inc' });
     // setCount((count) => count + 1);
     // setCount((count) => count + step);
   };
 
   const defineCount = function (e) {
-    dispatch(Number(e.target.value));
+    dispatch({ type: 'setCount', payload: Number(e.target.value) });
     // setCount(Number(e.target.value));
   };
 
